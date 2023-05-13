7ba35b288d5c4fcca7f0fa4a92ef1386bc719fa5
Jacqueline-Renito <al291218@edu.uaa.mx>
2023-05-12 22:26:32 -0600

    Inclusion del mapa

--- src/app/acercade/acercade.component.ts
+++ src/app/acercade/acercade.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Map, marker, tileLayer } from 'leaflet';
 @Component({
   selector: 'app-acercade',
   templateUrl: './acercade.component.html',
   styleUrls: ['./acercade.component.css']
 })
 export class AcercadeComponent {
+
+  ngAfterViewInit(): void{
+    const map = new Map('map').setView([21.85267,-102.32112], 13);
+    tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    const markerItem = marker([21.85267,-102.32112]).addTo(map).bindPopup("Farmacias Parecidas");
+
+    map.fitBounds([
+      [markerItem.getLatLng().lat, markerItem.getLatLng().lng]
+    ]);
+
+  }
 
 }
