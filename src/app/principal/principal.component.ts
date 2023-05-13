a5c8305ae446b2c1e4f023e4a2306d26ec3ef919
FabianMaEs <79736165+FabianMaEs@users.noreply.github.com>
2023-05-13 13:58:45 -0600

    Video

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -9,6 +9,10 @@ export class PrincipalComponent {
   selectedOption: string = "";
   feedback: string = "";
 
+  valores = ['oj3SSLj0Xyc', 'cxOnXZDpnD8', 'ySzldNXmMzc', 'zSqDbKk6ecs'];
+  indice = Math.floor(Math.random() * this.valores.length);
+  video = this.valores[this.indice];
+
   submit() {
     console.log(`Opción seleccionada: ${this.selectedOption}`);
     console.log(`Comentario: ${this.feedback}`);
