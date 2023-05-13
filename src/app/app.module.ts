b939e4ea03580d02d7c9f76a90bacea56d4d875e
Rafa Palacios <rafaelpj09@gmail.com>
2023-05-12 19:20:54 -0600

    Pagina Principal y cambio de rutas en img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import {MatCardModule} from '@angular/material/card';
-
-import {ServicioMService} from './servicio-m.service'
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ReporteCitasComponent } from './reporte-citas/reporte-citas.component';
@@ -13,13 +10,26 @@ import { RegistroCitasComponent } from './registro-citas/registro-citas.componen
 import { CitaMedicoComponent } from './cita-medico/cita-medico.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { FooterComponent } from './footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PortafolioComponent } from './portafolio/portafolio.component';
 import { AcercadeComponent } from './acercade/acercade.component';
 import { EnlaceComponent } from './enlace/enlace.component';
 
+// Angular Materials
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule} from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+// Servicio
+import {ServicioMService} from './servicio-m.service'
+
+// Andrew
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PortafolioComponent } from './portafolio/portafolio.component';
 //import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
@@ -33,7 +43,7 @@ import { PortafolioComponent } from './portafolio/portafolio.component';
     FooterComponent,
     AcercadeComponent,
     EnlaceComponent,
-    PortafolioComponent
+    PortafolioComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,7 +51,15 @@ import { PortafolioComponent } from './portafolio/portafolio.component';
     BrowserAnimationsModule,
     MatCardModule,
     FullCalendarModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [ ServicioMService],
   bootstrap: [AppComponent]
