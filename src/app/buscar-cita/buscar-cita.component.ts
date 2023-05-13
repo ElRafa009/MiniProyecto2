1785453f11e719f7ddff02f35ad73a31d8bf2282
FabianMaEs <79736165+FabianMaEs@users.noreply.github.com>
2023-05-13 12:24:38 -0600

    Update buscar-cita.component.ts

--- src/app/buscar-cita/buscar-cita.component.ts
+++ src/app/buscar-cita/buscar-cita.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-buscar-cita',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./buscar-cita.component.css']
 })
 export class BuscarCitaComponent {
+  nombre: string = '';
+  existe: boolean = false;
+  datosJSON: any;
+  datos: any;
+  constructor(private rutaActiva: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.nombre = this.rutaActiva.snapshot.params['nombre'];
+
+    this.rutaActiva.params.subscribe((params: Params) => {
+      this.nombre = params['nombre'];
+    });
+
+    this.datosJSON = localStorage.getItem('datos');
+    //console.log('Encontrado: ');
+    //console.log(this.datosJSON);
+
+    //Buscar nombre en datosJSON y mostrar
+
+    this.datos = JSON.parse(this.datosJSON);
+
+    /*let encontrado = datos.find((dato: { nombre: string; }) => dato.nombre === this.nombre);
+
+    if (encontrado) {
+      this.existe = true;
+    }*/
+
+    for (let dato of this.datos) {
+      console.log(dato.formularioContacto.nombre + " ? " + this.nombre);
+      if (dato.formularioContacto.nombre === this.nombre) {
+        this.existe = true;
+      }
+
+    }
+
+  }
 }
