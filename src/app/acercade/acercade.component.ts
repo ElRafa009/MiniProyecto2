6456295feb68f65d6710aa7f164d42e374fcf187
Jacqueline-Renito <al291218@edu.uaa.mx>
2023-05-12 22:58:53 -0600

    Binding en una direccion

--- src/app/acercade/acercade.component.ts
+++ src/app/acercade/acercade.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
 import { Map, marker, tileLayer } from 'leaflet';
+import { usuario } from '../../modelo/usuario';
 @Component({
   selector: 'app-acercade',
   templateUrl: './acercade.component.html',
   styleUrls: ['./acercade.component.css']
 })
 export class AcercadeComponent {
+
+  andrew: usuario = {
+    nombre: "Andrew",
+    descripcion: "Guapo sexy filántropo y millonario. Estudiante de la UAA de dia y batman de noche",
+  }
+
+  luis: usuario = {
+    nombre: "Luis Javier",
+    descripcion: "Estudiante de la UAA de la carrera de ing en sistemas computacionales del salón abajo de los baños que están cerca del jardín de las generaciones",
+  }
+
+  jacqueline: usuario = {
+    nombre: "Jacqueline",
+    descripcion: "Obsesionada con el color rosa y la boyband Big Time Rush, fan de gastar su dinero en merch de BTR",
+  }
+
+  fabian: usuario = {
+    nombre: "Fabian",
+    descripcion: "Inteligente, es como un murciélago, le gusta trabajar de noche, maneja uno de los medios de transporte mas peligrosos que existen en México y le vale.",
+  }
+
+  rafael: usuario = {
+    nombre: "Rafael",
+    descripcion: "Hombre melancólico, amante del cine y en sus tiempos libre se dedica a salvar el mundo, programador de día, hombre enmascarado de noche",
+  }
 
   ngAfterViewInit(): void{
     const map = new Map('map').setView([21.85267,-102.32112], 13);
