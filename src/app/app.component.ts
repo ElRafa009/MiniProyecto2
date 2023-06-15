5013337ed3c793feb5d99431677275ce03acc2d2
Andrew Bedoy <116923223+AndrewBedoy@users.noreply.github.com>
2023-06-15 12:46:16 -0600

    Ya meti la accesibilidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Accessibility } from 'accessibility/dist/main';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MiniProyectoWeb2';
+  labels: any;
+  constructor() {
+    var option = new Accessibility({
+      language: {
+        textToSpeechLang: 'es-ES',
+        speechToTextLang: 'es-ES'
+      },
+      labels:{
+        resetTitle: 'Reiniciar',
+        closeTitle: 'Cerrar',
+        menuTitle: 'Accesibilidad',
+        increaseText: 'Aumentar tamaño de letra',
+        decreaseText: 'Disminuir tamaño de letra',
+        increaseTextSpacing: 'Aumentar espaciado horizontal',
+        decreaseTextSpacing: 'Disinuir espaciado horizontal',
+        increaseLineHeight: 'Aumentar espaciado vertical',
+        decreaseLineHeight: 'Disminuir espaciado vertical',
+        invertColors: 'Invertir colores',
+        grayHues: 'Escala de Grises',
+        underlineLinks: 'Subrayar links',
+        bigCursor: ' Cursor más grande',
+        readingGuide: 'Guía de lectura',
+        textToSpeech:'Lector de pantalla',
+        speechToText: 'Dictado de texto',
+        disableAnimations: 'Desabilitar animaciones',
+        screenReader: 'Lector de pantalla'
+      },
+      icon: {
+
+        circular: true,
+        
+        img: 'accessibility',
+        
+        },
+      
+        
+    });
+    option.disableUnsupportedModules();
+  }
 }
