ca2990c900d97500350013c499a2f448899b2d1a
FabianMaEs <fabmac865@gmail.com>
2023-06-16 14:58:30 -0600

    QR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -33,6 +34,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BuscarCitaComponent } from './buscar-cita/buscar-cita.component';
 import { DomseguroPipe } from './domseguro.pipe';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { ContactoComponent } from './contacto/contacto.component';
+import { AyudaPreguntasComponent } from './ayuda-preguntas/ayuda-preguntas.component';
 //import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
@@ -49,6 +52,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     PortafolioComponent,
     BuscarCitaComponent,
     DomseguroPipe,
+    ContactoComponent,
+    AyudaPreguntasComponent,
   ],
   imports: [
     BrowserModule,
@@ -65,6 +70,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
+    HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,57 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
 
+  constructor(private http: HttpClient) {
+    window.onload = () => {
+      this.generarCodigoQR();
+    };
+  }
+  imagenCodigoUrl: string = '';
+
+  generarCodigoQR(): void {
+    console.log('Generar()');
+    // Solicitud HTTP al API
+    this.http
+      .get<any>('https://proyectofinal-apis.onrender.com/codigoqr')
+      .subscribe(
+        (respuesta) => {
+          // El código QR generado estará en la propiedad "url" de la respuesta
+          this.imagenCodigoUrl = respuesta.imagenCodigoUrl;
+          const canvas = document.getElementById('QR') as HTMLCanvasElement;
+          const context = canvas.getContext('2d');
+          const mensaje = document.getElementById('mensaje') as HTMLElement;
+
+          if (context) {
+            const img = new Image();
+            img.src = this.imagenCodigoUrl;
+
+            img.onload = () => {
+              canvas.width = img.width;
+              canvas.height = img.height;
+              context.drawImage(img, 0, 0);
+              canvas.style.display = 'block';
+              mensaje.style.display = 'block';
+              if (mensaje) mensaje.innerHTML = '¡Escanéame!';
+            };
+          } else {
+            console.error(
+              'No se pudo obtener el contexto de dibujo 2D del canvas.'
+            );
+            if (mensaje) mensaje.innerHTML = 'No se pudo generar el QR :(';
+          }
+        },
+        (error) => {
+          console.error(error);
+          const mensaje = document.getElementById('mensaje');
+          if (mensaje) mensaje.innerHTML = 'No se pudo acceder al API';
+        }
+      );
+  }
 }
